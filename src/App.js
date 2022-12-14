09ae363360d17fc86821e489c5bab96937253347
ZenitsuV <106746020+ZenitsuV@users.noreply.github.com>
2022-12-14 22:59:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Topnav from './nav/top';
+import Sidenav from './nav/side';
+import Summary from './reports/Summary';
+import Tracker from './reports/Tracker';
+import Profile from './reports/Profile';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Topnav></Topnav>
+      <Sidenav></Sidenav>
+      <main>
+        <Routes>
+          <Route path="/summary" element={<Summary />} />
+          <Route path="/tracker" element={<Tracker />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
---
+++ src/reports/Summary.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Summary = () => {
+  return <div className="report_container"> Summary report</div>;
+};
+
+export default Summary;
