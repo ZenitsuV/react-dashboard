58120adea33e13792f97f36910e29250bb48e1d5
ZenitsuV <106746020+ZenitsuV@users.noreply.github.com>
2022-12-21 22:04:40 +0530

    5 files modified, 1 added

--- src/components/PeriodToggle.js
+++ src/components/PeriodToggle.js
@@ -1,39 +1,59 @@
 import React from 'react';
 import './PeriodToggle.css';
 
-const PeriodToggle = () => {
+const PeriodToggle = ({ selectedPeriod, onSelect }) => {
   return (
     <div className="period_toggle">
       <ul style={{ width: '253px', height: '25px' }}>
         <li
-          class="SummaryTime TimeToggle"
-          valueGet="C01W"
-          id="SummaryTimeC01W{{id}}"
+          className={`${
+            selectedPeriod === 'CW'
+              ? 'SummaryTime TimeToggle TimeToggleSelected'
+              : 'SummaryTime TimeToggle'
+          }`}
+          valueGet="CW"
+          id="SummaryTimeC01W"
           style={{
             borderTopLeftRadius: '5px',
             borderBottomLeftRadius: '5px',
           }}
+          onClick={onSelect.bind(null, 'CW')}
         >
           CW
         </li>
         <li
-          class="SummaryTime TimeToggle"
+          className={`${
+            selectedPeriod === 'C4W'
+              ? 'SummaryTime TimeToggle TimeToggleSelected'
+              : 'SummaryTime TimeToggle'
+          }`}
           valueGet="C04W"
-          id="SummaryTimeC04W{{id}}"
+          id="SummaryTimeC04W"
+          onClick={onSelect.bind(null, 'C4W')}
         >
           C4W
         </li>
         <li
-          class="SummaryTime TimeToggle"
+          className={`${
+            selectedPeriod === 'C13W'
+              ? 'SummaryTime TimeToggle TimeToggleSelected'
+              : 'SummaryTime TimeToggle'
+          }`}
           valueGet="C13W"
-          id="SummaryTimeC13W{{id}}"
+          id="SummaryTimeC13W"
+          onClick={onSelect.bind(null, 'C13W')}
         >
           C13W
         </li>
         <li
-          class="SummaryTime TimeToggle"
+          className={`${
+            selectedPeriod === 'CQTD'
+              ? 'SummaryTime TimeToggle TimeToggleSelected'
+              : 'SummaryTime TimeToggle'
+          }`}
           valueGet="CQTD"
-          id="SummaryTimeCQTD{{id}}"
+          id="SummaryTimeCQTD"
+          onClick={onSelect.bind(null, 'CQTD')}
           style={{
             borderTopRightRadius: '5px',
             borderBottomRightRadius: '5px',
--- src/reports/Summary.js
+++ src/reports/Summary.js
@@ -2,11 +2,16 @@ import React, { useState } from 'react';
 import './Summary.css';
 import PeriodToggle from '../components/PeriodToggle';
 import KPIbox from '../components/KPIbox';
+import Chart from '../components/Chart';
 
 const Summary = () => {
   const [selectedKPI, setSelectedKPI] = useState('TRx');
+  const [selectedPeriod, setSelectedPeriod] = useState('CW');
+  const [selectedProduct, setSelectedProduct] = useState('Rollup');
 
-  console.log(selectedKPI);
+  const selectProductHandler = (product) => {
+    setSelectedProduct(product);
+  };
 
   return (
     <div className="report_container">
@@ -22,11 +27,14 @@ const Summary = () => {
               </div>
               <div className="pop_up_filter">Filter</div>
             </div>
-            <PeriodToggle />
+            <PeriodToggle
+              onSelect={setSelectedPeriod}
+              selectedPeriod={selectedPeriod}
+            />
           </div>
         </div>
         <div className="Date_wrapper">
-          <h3 id="Summary_Alerts{{id}}" className="DataasofDateText">
+          <h3 id="Summary_Alerts" className="DataasofDateText">
             Data as of
           </h3>
         </div>
@@ -36,7 +44,7 @@ const Summary = () => {
         <section className="kpi_section_inner_container">
           <div className="section_heading">
             <div className="section_heading_text">
-              <span>Performance Metrics</span>
+              <span>{`Performance Metrics - ${selectedPeriod}`}</span>
             </div>
           </div>
           <section className="kpi_inner">
@@ -154,24 +162,34 @@ const Summary = () => {
 
           <ul style={{ margin: '5px 10px 0 10px' }}>
             <li
-              class="Summary_PlotName"
+              className={`${
+                selectedProduct === 'Rollup'
+                  ? 'Summary_PlotName TimeToggleSelected'
+                  : 'Summary_PlotName'
+              }`}
               style={{
                 borderTopLeftRadius: '5px',
                 borderBottomLeftRadius: '5px',
               }}
               valueGet="Rollup"
-              id="SummaryRollupToggle{{id}}"
+              id="SummaryRollupToggle"
+              onClick={selectProductHandler.bind(null, 'Rollup')}
             >
               Rollup
             </li>
             <li
-              class="Summary_PlotName"
+              className={`${
+                selectedProduct === 'Family'
+                  ? 'Summary_PlotName TimeToggleSelected'
+                  : 'Summary_PlotName'
+              }`}
               style={{
                 borderTopTightRadius: '5px',
                 borderBottomRightRadius: '5px',
               }}
               valueGet="Family"
-              id="SummaryFamilyToggle{{id}}"
+              id="SummaryFamilyToggle"
+              onClick={selectProductHandler.bind(null, 'Family')}
             >
               Family
             </li>
@@ -200,6 +218,9 @@ const Summary = () => {
               class="VisualType rsm_GridIcon"
             ></div>
           </div>
+        </div>
+        <div id="SummaryTrendChart" class="Summary_TrendChart">
+          <Chart />
         </div>
       </section>
     </div>
